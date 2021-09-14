79125df007607c3c4e1364d352557ba93d9cf430
aleksei@mazur <mazuralexey@gmail.com>
2021-09-14 03:20:50 +0300

    step 3 - TimedRotationFileHAndler for server

--- logs/configs/server_log_config.py
+++ logs/configs/server_log_config.py
@@ -1,19 +1,19 @@
-import logging
+import logging.handlers
 import sys
 import os
+
+"""
+Конфигурация логгера для сервера:
+Форматтер
+Обработчик
+Логгер
+Настройки
+"""
+
+# Подготовка имени файла
 sys.path.append('../')
-
-"""
-Конфигурация логгера для сервера
-"""
-
-# Подготовка имени файла для логирования
 PATH = os.path.dirname(os.path.abspath(__file__))
 PATH = os.path.join(PATH, 'server.log')
-
-# Создаем экземпляр объекта класса Logger
-Server_logger = logging.getLogger('server')
-Server_logger.setLevel(logging.DEBUG)
 
 # Формат сообщений: "<дата-время> <уровень_важности> <имя_модуля> <сообщение>"
 s_formatter = logging.Formatter('%(asctime)-27s %(levelname)-12s %(filename)-24s %(message)s')
@@ -21,22 +21,32 @@ s_formatter = logging.Formatter('%(asctime)-27s %(levelname)-12s %(filename)-24s
 # Создаем обработчик логгирования для отладки, устанавливаем уровень важности, назначаем формат
 stream_handler = logging.StreamHandler(sys.stderr)
 stream_handler.setFormatter(s_formatter)
-stream_handler.setLevel(logging.WARNING)
+stream_handler.setLevel(logging.ERROR)
 
+"""
+TimedRotatingFileHandler
+В аргументе interval передается число,
+определяющее величину интервала в единицах, а в when — строка,
+определяющая единицы измерения.
+Допустимыми значениями для аргумента when являются:
+S (секунды), M (минуты), H (часы), D (дни), W (недели) и midnight (ротация выполняется в полночь)."""
 
-# Создать обработчик для вывода сообщений в лог-файл
-file_handler = logging.FileHandler(PATH, encoding='utf-8')
+file_handler = logging.handlers.TimedRotatingFileHandler(PATH, encoding='utf-8',
+                                                         interval=1, when='midnight')
 file_handler.setFormatter(s_formatter)
 file_handler.setLevel(logging.DEBUG)
+
+# Создаем экземпляр объекта класса Logger
+Server_logger = logging.getLogger('server')
 
 # Добавляем в логгеры обработчики событий и устанавливаем уровень логгирования
 Server_logger.addHandler(stream_handler)
 Server_logger.addHandler(file_handler)
-Server_logger.setLevel(logging.INFO)
+Server_logger.setLevel(logging.DEBUG)
 
 # отладка
 if __name__ == '__main__':
-    Server_logger.info('Важная информация')
+    Server_logger.info('Важная? информация')
     Server_logger.debug('Отладочное сообщение')
     Server_logger.warning('Предупреждение!')
     Server_logger.error('Где-то ошибка!!')
