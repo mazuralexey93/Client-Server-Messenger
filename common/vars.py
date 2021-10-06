ab02281716026709df42874a5f76bad5828821e9
aleksei@mazur <mazuralexey@gmail.com>
2021-10-06 23:08:52 +0300

    step 6 - client threading logic

--- common/vars.py
+++ common/vars.py
@@ -17,9 +17,19 @@ EVENT = 'event'
 TIME = 'time'
 USER = 'user'
 ACCOUNT_NAME = 'account_name'
+SENDER = 'from'
+DESTINATION = 'to'
+
 PRESENCE = 'presence'
 RESPONSE = 'response'
 ERROR = 'error'
-SENDER = 'sender'
 MESSAGE = 'message'
 MESSAGE_TEXT = 'message_text'
+EXIT = 'exit'
+
+"""  Словари - ответы: """
+RESPONSE_200 = {RESPONSE: 200}
+RESPONSE_400 = {
+    RESPONSE: 400,
+    ERROR: None
+}
