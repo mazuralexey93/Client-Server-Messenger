e4a02749f2746d30508779be62d0ff21624198db
mazuralexey93 <mazuralexey93@gmail.com>
2021-10-28 23:56:03 +0300

    posix adaptive

--- launcher.py
+++ launcher.py
@@ -13,14 +13,11 @@ while True:
     if mode.lower() == 'q':
         break
     elif mode.lower() == 's':
-        process_list.append(subprocess.Popen(f'python server.py',
-                                             creationflags=subprocess.CREATE_NEW_CONSOLE))
-        process_list.append(subprocess.Popen('python client.py -n client1',
-                                             creationflags=subprocess.CREATE_NEW_CONSOLE))
-        process_list.append(subprocess.Popen('python client.py -n client2',
-                                             creationflags=subprocess.CREATE_NEW_CONSOLE))
-        process_list.append(subprocess.Popen('python client.py -n client3',
-                                             creationflags=subprocess.CREATE_NEW_CONSOLE))
+
+        process_list.append(subprocess.Popen(f'server.py'))
+        process_list.append(subprocess.Popen('python client.py -n client1'))
+        process_list.append(subprocess.Popen('python client.py -n client2'))
+        process_list.append(subprocess.Popen('python client.py -n client3'))
     elif mode.lower() == 'e':
         while process_list:
             process_to_delete = process_list.pop()
